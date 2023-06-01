936269d6d11809bffb9b80df2584b028ee32ee96
TheHuKoJlaU <kuznetso.nikolai@gmail.com>
2023-06-01 13:58:29 +0400

    Added comments

--- FigureByTrajectory/DrawFigure.cs
+++ FigureByTrajectory/DrawFigure.cs
@@ -6,17 +6,30 @@ using System.Threading.Tasks;
 
 namespace FigureByTrajectory
 {
+    /// <summary>
+    /// Класс для расчёта точек пятиконечной звезды
+    /// </summary>
     internal class DrawFigure
     {
+        /// <summary>
+        /// Статический метод для расчёта точек пятиконечной звезды
+        /// </summary>
+        /// <param name="xc">X-центра поля</param>
+        /// <param name="yc">Y-центра поля</param>
+        /// <param name="radius">Радиус фигуры</param>
+        /// <param name="diameter">Диаметр траектории</param>
+        /// <param name="speed">Скорость движения фигуры</param>
+        /// <returns>Точки с координатами отрисовки фигуры</returns>
         static public PointF[] calculatePointsFigure(int xc, int yc, int radius, int diameter, double speed)
         {
-            PointF[] points = new PointF[11];
-            double a = 50, da = Math.PI / 5, l;
+            PointF[] points = new PointF[11];   // инициализируем массив floating-точек
+            double a = 50, da = Math.PI / 5, l; // задаём начальные значения
             for (int k = 0; k < 11; k++)
             {
-                l = k % 2 == 0 ? radius : radius / 2;
+                l = k % 2 == 0 ? radius : radius / 2;   // проверяем, во внешнем или во внутреннем радиусе ставить точку
+                // добавляем в массив новую точку по координатам, расчитанным по формуле
                 points[k] = new PointF((float)(diameter / 2 * Math.Cos(speed) + xc + l * Math.Cos(a)), (float)(diameter / 2 * Math.Sin(speed) + yc + l * Math.Sin(a)));
-                a += da;
+                a += da; // увеличиваем угол
             }
 
             return points;
--- FigureByTrajectory/Form1.cs
+++ FigureByTrajectory/Form1.cs
@@ -13,6 +13,7 @@ namespace FigureByTrajectory
 {
     public partial class Form1 : Form
     {
+		// Инициализируем переменные со значениями из файла конфигурации
         Pen trajectoryPen = new Pen(Properties.Settings.Default.trajectoryColor, 1);
         Pen figurePen = new Pen(Properties.Settings.Default.figureColor, 1);
         int trajectoryDiameter = Properties.Settings.Default.trajectoryDiameter;
@@ -24,14 +25,22 @@ namespace FigureByTrajectory
 
         public Form1()
         {
-            InitializeComponent();
-            xc = pictureBox1.Width / 2;
+            InitializeComponent();          // инициализируем стандарные компоненты WinForms
+            // Задаём координаты центра блока рисования
+            xc = pictureBox1.Width / 2;     
             yc = pictureBox1.Height / 2;
-            DoubleBuffered = true;
+
+            DoubleBuffered = true;         // Включаем двойную буферизацию для решения проблем мерцания, связанных с несколькими операциями рисования
         }
 
+        /// <summary>
+        /// Метод, срабатывающий при загрузке формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
+			// задаем цвет и положения trackBar`ов относительно значений в файле конфигурации при загрузке формы
             pictureBox1.BackColor = Properties.Settings.Default.BGColor;
             trajectorySize.Value = Properties.Settings.Default.trajectoryDiameter;
             figureBreathingRate.Value = Properties.Settings.Default.figureBreathingRate;
@@ -39,9 +48,14 @@ namespace FigureByTrajectory
             figureSpeed.Value = Properties.Settings.Default.figureSpeed;
         }
 
-
+        /// <summary>
+        /// Метод, срабатывающий при закрытии формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
+			// сохраняем в файлы конфигурации значения при закрытии формы
             Properties.Settings.Default.figureRadius = figureRadius;
             Properties.Settings.Default.breathingRate = breathingRate;
             Properties.Settings.Default.figureSize = figureSize.Value;
@@ -51,16 +65,26 @@ namespace FigureByTrajectory
             Properties.Settings.Default.Save();
         }
 
+        /// <summary>
+        /// Метод, который срабатывает при нажатии на кнопку "Цвет фона"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonBGColor_Click(object sender, EventArgs e)
         {
-            colorDialogBG.Color = Properties.Settings.Default.BGColor;
-            colorDialogBG.FullOpen = true;
-            if (colorDialogBG.ShowDialog() == DialogResult.Cancel)
-                return;
-            Properties.Settings.Default.BGColor = colorDialogBG.Color;
-            pictureBox1.BackColor = colorDialogBG.Color;
+            colorDialogBG.Color = Properties.Settings.Default.BGColor;  // подтягиваем цвет фона из настроек
+            colorDialogBG.FullOpen = true;                              // открываем диалог выбора цвета
+            if (colorDialogBG.ShowDialog() == DialogResult.Cancel)      // проверяем, как был закрыт диалог
+                return;                                                 // если крестиком, то ничего не делая, закрываем диалог
+            Properties.Settings.Default.BGColor = colorDialogBG.Color;  // иначе записываем новый цвет в настройки
+            pictureBox1.BackColor = colorDialogBG.Color;                // окрашиваем область рисования в выбранный цвет
         }
 
+        /// <summary>
+        /// Метод, который срабатывает при нажатии на кнопку "Цвет траектории"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonTrajectoryColor_Click(object sender, EventArgs e)
         {
             colorDialogTrajectory.Color = Properties.Settings.Default.trajectoryColor;
@@ -68,9 +92,14 @@ namespace FigureByTrajectory
             if (colorDialogTrajectory.ShowDialog() == DialogResult.Cancel)
                 return;
             Properties.Settings.Default.trajectoryColor = colorDialogTrajectory.Color;
-            trajectoryPen = new Pen(colorDialogTrajectory.Color, 1);
+            trajectoryPen = new Pen(colorDialogTrajectory.Color, 1);                    // создаем карандаш для рисования фигуры с выбранным цветом
         }
 
+        /// <summary>
+        /// Метод, который срабатывает при нажатии на кнопку "Цвет фигуры"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonFigureColor_Click(object sender, EventArgs e)
         {
             colorFigure.Color = Properties.Settings.Default.figureColor;
@@ -81,35 +110,56 @@ namespace FigureByTrajectory
             figurePen = new Pen(colorFigure.Color, 1);
         }
 
+        /// <summary>
+        /// Метод, который срабатывает при изменении trackBar`a "Размер траектории"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void trajectorySize_ValueChanged(object sender, EventArgs e)
         {
-            trajectoryDiameter = trajectorySize.Value;
+            trajectoryDiameter = trajectorySize.Value;  // получаем значение из trackBar`a изаписываем в переменную для дальнейшего рисования
         }
 
+        /// <summary>
+        /// Метод, вызываемый таймером с интервалом 40мс
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void timerFigureSpeed_Tick(object sender, EventArgs e)
         {
+            // проверяем увеличивается или уменьшаеся фигура
             if (breathingRate)
             {
+                // увеличиваем фигуру
                 figureRadius += figureSize.Value / figureBreathingRate.Value;
-                if (figureRadius >= figureSize.Value * 2)
+                if (figureRadius >= figureSize.Value * 2)   // проверяем, больше или равен радиус фигуры, указанному в trackBar`e размеру
                     breathingRate = false;
             }
             else
             {
+                // уменьшаем фигуру
                 figureRadius -= figureSize.Value / figureBreathingRate.Value;
                 if (figureRadius <= 10)
                     breathingRate = true;
             }
 
+            // скорость движения фигуры по траектории
             if (speed > 2 * Math.PI) speed = 0.0;
             speed += (double)figureSpeed.Value / 10;
+
+            // стандартная функция, которая сообщает приложению, что нужно вызвать метод перерисовки
             Invalidate(true);
         }
 
+        /// <summary>
+        /// Метод для автоматического рисования при изменении любого из параметров отрисовки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawEllipse(trajectoryPen, xc - trajectoryDiameter / 2, yc - trajectoryDiameter / 2, trajectoryDiameter, trajectoryDiameter);
-            e.Graphics.DrawLines(figurePen, DrawFigure.calculatePointsFigure(xc, yc, figureRadius, trajectoryDiameter, speed));
+            e.Graphics.DrawEllipse(trajectoryPen, xc - trajectoryDiameter / 2, yc - trajectoryDiameter / 2, trajectoryDiameter, trajectoryDiameter);    // рисуем траекторию (круг)
+            e.Graphics.DrawLines(figurePen, DrawFigure.calculatePointsFigure(xc, yc, figureRadius, trajectoryDiameter, speed));                         // рисуем фигуру (пятиконечную звезду)
         }
     }
 }
